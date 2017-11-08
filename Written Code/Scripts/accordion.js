042879790069eca9c96e667c221624ed325ee893
Alex Schor <peanut.covered.in.wasabi@gmail.com>
2017-11-08 14:39:34 -0700

    fixed delay
    the issue was that in the setTimeout the "this" keyword referred to something else for some reason.

--- Written Code/Scripts/accordion.js
+++ Written Code/Scripts/accordion.js
@@ -1,32 +1,18 @@
 var animating = false;
-var wait = 200;
+var wait = 10;
 $(function () {
     'use strict';
     $('.item').first().addClass('active-accordion');
     $('.info').first().show().animate({ width: '800px' });
     $('.item').click(function () {
-        if (!$(this).hasClass("active-accordion") && !animating) {
-            $(this).addClass("active-accordion").siblings('.item').removeClass('active-accordion');
-            animating = true;
-            // setTimeout(function() {
-            //setTimeout wasn't fucking working, so it is commented out, adding the delay function.
-            delay(wait);
-            $(this).next().show().animate({ width: '800px' }).siblings('.info').animate({ width: 0 }, function () {
-                $(this).hide();
-                animating = false;
-            });
-            // }, 10);
+        if(!$(this).hasClass("active-accordion") && !animating){
+          $(this).addClass("active-accordion").siblings('.item').removeClass('active-accordion');
+          animating = true;
+          $(this).next().siblings('.info').animate({width: 0}, 500, function() { $(this).hide();});
+          setTimeout(function() {
+            $(".active-accordion").next().show();
+            $(".active-accordion").next().animate({width:'800px'}, 500, function() { animating = false; });
+          }, wait);
         }
     });
 });
-
-
-// hackey way to put in a delay(since setTimeout wasn't working)
-function delay(delayTime) {
-    var start = new Date().getTime();
-    for (var i = 0; i < 1e7; i++) {
-        if ((new Date().getTime() - start) > delayTime) {
-            break;
-        }
-    }
-}
