455b0ceaa0103335ce955fc4e1542ab7c1673e9e
Angelbots1339 <angelbots1339@gmail.com>
2017-11-07 20:45:26 -0700

    Added Bebas Neue font and history accordion
    not sure if Bebas Neue Regular is correct font, maybe a heavier weight?

--- Written Code/Scripts/accordion.js
+++ Written Code/Scripts/accordion.js
@@ -1,3 +1,11 @@
-function accTabOpen(evn, tabName) {
-
-}
+$(function () {
+	'use strict';
+    $('.item').first().addClass('active-accordion');
+    $('.info').first().show().animate({width: '800px'});
+    $('.item').click(function () {
+        $(this).addClass("active-accordion").siblings('.item').removeClass('active-accordion');
+        $(this).next().show().animate({width: '800px'}).siblings('.info').animate({width: 0}, function () {
+            $(this).hide();
+        });
+    });
+});
