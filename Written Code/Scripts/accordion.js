ae6da8290735d0169f7d2c7acf71f53c2c10dff4
Angelbots1339 <angelbots1339@gmail.com>
2017-11-07 20:56:02 -0700

    accordion bugfix
    now cannot animate twice at once, which is good. animation race
    condition still exists.

--- Written Code/Scripts/accordion.js
+++ Written Code/Scripts/accordion.js
@@ -1,11 +1,16 @@
+var animating = false;
 $(function () {
 	'use strict';
     $('.item').first().addClass('active-accordion');
     $('.info').first().show().animate({width: '800px'});
     $('.item').click(function () {
-        $(this).addClass("active-accordion").siblings('.item').removeClass('active-accordion');
-        $(this).next().show().animate({width: '800px'}).siblings('.info').animate({width: 0}, function () {
-            $(this).hide();
-        });
+    	if(!$(this).hasClass("active-accordion") && !animating){
+	        $(this).addClass("active-accordion").siblings('.item').removeClass('active-accordion');
+	        animating = true;
+	        $(this).next().show().animate({width: '800px'}).siblings('.info').animate({width: 0}, function () {
+	            $(this).hide();
+	            animating = false;
+	        });
+	    }
     });
 });
