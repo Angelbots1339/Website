a13bb6ef603e0d51e64d3984e30d3f29e8e5607a
SpadeSeveren <Jbolzweber@gmail.com>
2017-11-08 14:00:00 -0700

    Just formatting

--- Written Code/Scripts/accordion.js
+++ Written Code/Scripts/accordion.js
@@ -3,19 +3,19 @@ var wait = 200;
 $(function () {
     'use strict';
     $('.item').first().addClass('active-accordion');
-    $('.info').first().show().animate({width: '800px'});
+    $('.info').first().show().animate({ width: '800px' });
     $('.item').click(function () {
-        if(!$(this).hasClass("active-accordion") && !animating){
+        if (!$(this).hasClass("active-accordion") && !animating) {
             $(this).addClass("active-accordion").siblings('.item').removeClass('active-accordion');
             animating = true;
             // setTimeout(function() {
             //setTimeout wasn't fucking working, so it is commented out, adding the delay function.
             delay(wait);
-            $(this).next().show().animate({width: '800px'}).siblings('.info').animate({width: 0}, function () {
+            $(this).next().show().animate({ width: '800px' }).siblings('.info').animate({ width: 0 }, function () {
                 $(this).hide();
                 animating = false;
             });
-        // }, 10);
+            // }, 10);
         }
     });
 });
@@ -25,8 +25,8 @@ $(function () {
 function delay(delayTime) {
     var start = new Date().getTime();
     for (var i = 0; i < 1e7; i++) {
-      if ((new Date().getTime() - start) > delayTime){
-        break;
-      }
+        if ((new Date().getTime() - start) > delayTime) {
+            break;
+        }
     }
-  }
+}
